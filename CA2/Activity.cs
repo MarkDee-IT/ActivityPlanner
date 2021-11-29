e35834c51bc402eeb354f3c6e7dcfe7bbe29f3a5
MarkDee-IT <78158784+MarkDee-IT@users.noreply.github.com>
2021-11-29 20:23:57 +0000

    Added activity list to the listbox, and managed to get description text block working

---
+++ CA2/Activity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Activity : IComparable<Activity>
+    {
+        public enum ActivityType { Air, Water, Land }
+
+        public string Name { get; set; }
+
+        public DateTime ActivityDate { get; set; }
+
+        public decimal Cost { get; set; }
+
+        private string _description;
+
+        // Description automatically appends the cost to the end
+        public string Description
+        {
+            get { return _description + $" Cost - {Cost:C}"; }
+            set { _description = value; }
+        }
+
+        // Private attribute for the sake of allowing the activity type enum to be in the constructor
+        private ActivityType _activityType;
+
+        public Activity(string name, DateTime date, ActivityType typeOfActivity, decimal cost, string description)
+        {
+            Name = name;
+            ActivityDate = date;
+            _activityType = typeOfActivity;
+            Cost = cost;
+            _description = description;
+        }
+
+        public int CompareTo(Activity that)
+        {
+            if (this.ActivityDate < that.ActivityDate) return -1;
+            if (this.ActivityDate == that.ActivityDate) return 0;
+            return 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {ActivityDate.ToShortDateString()}";
+        }
+    }
+}
--- CA2/MainWindow.xaml.cs
+++ CA2/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace CA2
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Initialise list
+        List<Activity> allActivities = new List<Activity>()
+        {
+            new Activity("Hiking", new DateTime(2022, 02, 28), Activity.ActivityType.Land, 59.95m, "Walk up mountainous terrain"),
+            new Activity("Spelunking", new DateTime(2022, 04, 21), Activity.ActivityType.Land, 239.95m, "Explore deep and dark caves"),
+            new Activity("Cycling", new DateTime(2022, 05, 07), Activity.ActivityType.Land, 79.95m, "Take a relaxing bike ride across the countryside"),
+
+            new Activity("Kayaking", new DateTime(2022, 01, 10), Activity.ActivityType.Water, 199.95m, "Pilot a small craft down winding rivers"),
+            new Activity("Surfing", new DateTime(2022, 03, 06), Activity.ActivityType.Water, 159.95m, "Ride the waves of the ocean"),
+            new Activity("Diving", new DateTime(2022, 06, 14), Activity.ActivityType.Water, 119.95m, "Explore the ocean floor"),
+
+            new Activity("Skydiving", new DateTime(2022, 08, 13), Activity.ActivityType.Air, 299.95m, "Experience the world the only we know how, by jumping out of an operational aircraft"),
+            new Activity("Paragliding", new DateTime(2022, 02, 17), Activity.ActivityType.Air, 149.95m, "Relax by floating in the air behind a boat"),
+            new Activity("Wingsuit", new DateTime(2022, 06, 14), Activity.ActivityType.Air, 249.95m, "Soar through the air at high speeds and low altitudes")
+    };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AllActivities_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            allActivities.Sort();
+
+            AllActivities.ItemsSource = allActivities;
+        }
+
+        private void AllActivities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Defines the current activity selected as an activity, and displays the description in the description box
+            var activity = (Activity)AllActivities.SelectedItem;
+            Description.Text = activity.Description;
         }
     }
 }
